8dcfdab6e50d64aec6c386c91df27c4e02d11f2b
Thiago Costa <tjthiagocosta@gmail.com>
2022-03-06 22:16:21 -0300

    test: ensure EmailValidator throws if no email is provided

--- src/utils/helpers/email-validator.js
+++ src/utils/helpers/email-validator.js
@@ -1,7 +1,11 @@
+const MissingParamError = require('../errors/missing-param-error')
 const validator = require('validator')
 
 module.exports = class EmailValidator {
   isValid (email) {
+    if (!email) {
+      throw new MissingParamError('email')
+    }
     return validator.isEmail(email)
   }
 }
--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,6 @@
-const EmailValidator = require('./email-validator')
 const validator = require('validator')
+const MissingParamError = require('../errors/missing-param-error')
+const EmailValidator = require('./email-validator')
 
 const makeSut = () => {
   return new EmailValidator()
@@ -21,5 +22,10 @@ describe('Email Validator', () => {
     const sut = makeSut()
     sut.isValid('any_email@mail.com')
     expect(validator.email).toBe('any_email@mail.com')
+  })
+
+  test('should throw if no email is provided', async () => {
+    const sut = makeSut()
+    expect(() => { sut.isValid() }).toThrow(new MissingParamError('email'))
   })
 })
