1cf7f7438bdaf53910b5258fb9e6394aeafbdfb2
Thiago Costa <tjthiagocosta@gmail.com>
2022-03-08 08:44:41 -0300

    test: ensure LoadUserByEmailRepository returns an user if user is found

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['**/src/**/*.js'],
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
+  preset: '@shelf/jest-mongodb'
 }
