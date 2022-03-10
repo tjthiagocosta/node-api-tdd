0675569de21f24c069a490016f5af05511c1757c
Thiago Costa <tjthiagocosta@gmail.com>
2022-03-10 13:53:09 -0300

    test: ensure LoadUserByEmailRepository returns the correct user when user is found

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -7,7 +7,13 @@ class LoadUserByEmailRepository {
   }
 
   async load (email) {
-    const user = await this.userModel.findOne({ email })
+    const user = await this.userModel.findOne({
+      email
+    }, {
+      projecttion: {
+        password: 1
+      }
+    })
     return user
   }
 }
@@ -46,10 +52,14 @@ describe('LoadUserByEmail Repository', () => {
 
   test('should return an user if user is found', async () => {
     const { sut, userModel } = makeSut()
-    await userModel.insertOne({
-      email: 'valid_email@mail.com'
+    const fakeUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      name: 'any_name',
+      age: 50,
+      state: 'any_state',
+      password: 'hashed_password'
     })
     const user = await sut.load('valid_email@mail.com')
-    expect(user.email).toBe('valid_email@mail.com')
+    expect(user._id).toEqual(fakeUser.insertedId)
   })
 })
