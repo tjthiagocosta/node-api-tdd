ac9bd6a8c25d30cdfea799c0cdb4e7e431669235
Thiago Costa <tjthiagocosta@gmail.com>
2022-03-12 18:48:43 -0300

    refactor: move mongodb setup to a separated file

---
+++ src/infra/helpers/mongo-helper.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb')
+
+module.exports = {
+  async connect (uri, dbName) {
+    this.uri = uri
+    this.dbName = dbName
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    this.db = this.client.db(dbName)
+  },
+
+  async disconnect () {
+    await this.client.close()
+  },
+
+  async getDb () {
+    return this.db
+  }
+}
