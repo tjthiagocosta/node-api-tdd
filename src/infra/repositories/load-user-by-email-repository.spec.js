86373cd6ebee321adf6fb71b8d80a141c3f56384
Thiago Costa <tjthiagocosta@gmail.com>
2022-03-13 18:54:01 -0300

    test: ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -43,4 +43,10 @@ describe('LoadUserByEmail Repository', () => {
     const user = await sut.load('valid_email@mail.com')
     expect(user._id).toEqual(fakeUser.insertedId)
   })
+
+  test('should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@mail.com')
+    expect(promise).rejects.toThrow()
+  })
 })
